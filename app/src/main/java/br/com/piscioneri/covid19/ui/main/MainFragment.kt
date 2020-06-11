f645f7e61b41e1ea54051c6b8c035c252910a2e6
Matheus Piscioneri <matheus.piscioneri@luizalabs.com>
2020-06-11 15:23:56 -0300

    create layout main

--- app/src/main/java/br/com/piscioneri/covid19/ui/main/MainFragment.kt
+++ app/src/main/java/br/com/piscioneri/covid19/ui/main/MainFragment.kt
@@ -1,23 +1,30 @@
 package br.com.piscioneri.covid19.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import br.com.piscioneri.covid19.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
+        val list = arrayOf("Mundo", "Brasil", "Acre", "São Paulo", "Rio de Janeiro")
     }
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
@@ -25,6 +32,18 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            context!!,
+            android.R.layout.simple_list_item_1,
+            list
+        )
+        dropdown.setAdapter(adapter)
+        dropdown.setText(list[0], false)
+        dropdown.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            val selected = parent.adapter.getItem(position)
+            Toast.makeText(context, selected.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 
 }
